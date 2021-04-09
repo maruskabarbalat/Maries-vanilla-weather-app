f5dbfec6d8619fc365d518436d32679713f743c7
maruskabarbalat <77684914+maruskabarbalat@users.noreply.github.com>
2021-04-09 12:42:14 -0400

    added search engine

--- src/app.js
+++ src/app.js
@@ -44,10 +44,20 @@ function displayTemperature(response){
  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function search(city) {
+    let apiKey = "bf89fdb8b1b434c3446ff9fd63c2f493";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
 
 
-let apiKey = "bf89fdb8b1b434c3446ff9fd63c2f493";
-let city ="New York"
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+search("New York");
 
-axios.get(apiUrl).then(displayTemperature);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
